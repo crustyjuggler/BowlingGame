0c93748989ccc15841246536ced1974fe0ee2518
Damon Scott <damon-s@att.net>
2015-05-28 00:21:02 -0400

    Refactored Test class
    Made the Game object global and initialized it before every test. Added
    a RollMany method to handle multiple rolls of the same value.

--- BowlingGameTest/BowlingGameTest.cs
+++ BowlingGameTest/BowlingGameTest.cs
@@ -7,26 +7,34 @@ namespace BowlingGameTest
     [TestClass]
     public class BowlingGameTest
     {
+        private Game g;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            g = new Game();
+        }
+
         [TestMethod]
         public void TestRollAllGutterGame()
         {
-            Game g = new Game();
-            for (int i = 0; i < 20; i++)
-            {
-                g.Roll(0);
-            }
+            RollMany(20, 0);
             Assert.AreEqual(0, g.Score());
         }
 
         [TestMethod]
         public void TestRollAllOnesGame()
         {
-            Game g = new Game();
-            for (int i = 0; i < 20; i++)
-            {
-                g.Roll(1);
-            }
+            RollMany(20, 1);
             Assert.AreEqual(20, g.Score());
+        }
+
+        private void RollMany(int rolls, int pins)
+        {
+            for (int i = 0; i < rolls; i++)
+            {
+                g.Roll(pins);
+            }
         }
     }
 }
