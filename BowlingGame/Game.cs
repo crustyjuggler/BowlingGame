09d1043c15635a07bc2303bdb1fa6dca15c39e8d
Damon Scott <damon-s@att.net>
2015-05-28 00:16:24 -0400

    Added test for rolling all ones
    Tested rolling all ones in a game. Refactored Game class to add each
    roll to the running score.

--- BowlingGame/Game.cs
+++ BowlingGame/Game.cs
@@ -8,14 +8,20 @@ namespace BowlingGame
 {
     public class Game
     {
+        private List<int> rolls = new List<int>();
+
         public void Roll(int pins)
         {
-
+            rolls.Add(pins);
         }
 
         public int Score()
         {
             int score = 0;
+            for (int i = 0; i < rolls.Count; i++)
+            {
+                score += rolls[i];
+            }
             return score;
         }
     }
--- BowlingGameTest/BowlingGameTest.cs
+++ BowlingGameTest/BowlingGameTest.cs
@@ -17,5 +17,16 @@ namespace BowlingGameTest
             }
             Assert.AreEqual(0, g.Score());
         }
+
+        [TestMethod]
+        public void TestRollAllOnesGame()
+        {
+            Game g = new Game();
+            for (int i = 0; i < 20; i++)
+            {
+                g.Roll(1);
+            }
+            Assert.AreEqual(20, g.Score());
+        }
     }
 }
